a508087459772f838dd8d66db63fafdc82ea04b8
mateustavaresw <mateustavareszx007@gmail.com>
2026-09-02 14:28:13 -0300

    adicionando entrada de dados do usuário

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ def custo_total(x,cf,cv):
     return cf + cv * x
 def custo_medio(x,cf,cv):
     return custo_total(x,cf,cv)/x
+cf = float(input("digite o custo fixo: "))
+cv = float(input("digite o custo variável: "))
+x = float(input("digite a quantidade produzida: "))
